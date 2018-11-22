862f0260b05097a1ce2d0f365f1a01194d249ff4
Helmi Fraser <helmifraser1@gmail.com>
2018-11-22 18:17:33 +0000

    Implemented 'folder' argument, can now pass a folder containing videos. Implemented progress bar for progress through video set.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import sys
+import sys, os
 try:
     sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
 except Exception as e:
@@ -9,7 +9,7 @@ import argparse
 parser = argparse.ArgumentParser(description='Hazard video labelling tool')
 parser.add_argument('--filepath', metavar='file(s)', type=str, nargs='+', default=None,
 help='filepath(s) of videos to be analysed (provide absolute paths)')
-parser.add_argument('--folder', metavar='folder', type=str, nargs=1, default=None,
+parser.add_argument('--folder', metavar='folder', type=str, default=None,
 help='path to folder containing videos (provide absolute path)')
 parser.add_argument('--dest', metavar='destination', type=str, nargs=1, default='.',
 help='path to desired output destination (default: where this script is)')
@@ -29,6 +29,8 @@ from pyforms.controls   import ControlPlayer
 from pyforms.controls   import ControlButton
 from pyforms.controls   import ControlEventTimeline
 from pyforms.controls   import ControlDockWidget
+from pyforms.controls   import ControlProgress
+
 
 from AnyQt import QtCore
 
@@ -42,9 +44,6 @@ class VideoWindow(BaseWidget):
                         "dest": DEST
                         }
 
-        self._hazard_counter = 0
-        self._video_count = len(self._args["filepath"])
-        self._current_video = 0
         self.set_margin(10)
 
         #Definition of the forms fields
@@ -54,13 +53,31 @@ class VideoWindow(BaseWidget):
         self._player = ControlPlayer('Player')
         self._timeline = ControlEventTimeline('Timeline')
         self._panel = ControlDockWidget(label='Timeline', side='bottom', margin=10)
-        self._panel.value = self._timeline
         self._status = ControlText('Status')
+        self._file_list = []
+
+        if self._args["folder"] is None:
+            if self._args["filepath"] is not None:
+                self._file_list = self._args["filepath"]
+        elif self._args["folder"] is not None:
+            if os.path.isdir(self._args["folder"]):
+                self.__updateStatus("Source folder found at: {}".format(self._args["folder"]))
+                for (dirpath, dirnames, filenames) in os.walk(self._args["folder"]):
+                    path = [dirpath + "/" + f for f in filenames]
+                    self._file_list.extend(path)
+
+        self._video_count = len(self._file_list)
+        self._progress = ControlProgress(label="Video %p of " + str(self._video_count), defaultValue=1, min=1, max=self._video_count)
+
+        self._hazard_counter = 0
+        self._current_video = 0
 
         #Define function calls on button presses
         self._videofile.changed_event = self.__videoFileSelectionEvent
         self._hazardbutton.value = self.__labelHazard
         self._next.value = self.__nextVideo
+        self._panel.value = self._timeline
+        self._progress.value = self._current_video + 1
 
         #Define events
         self._player.process_frame_event = self.__processFrame
@@ -73,45 +90,51 @@ class VideoWindow(BaseWidget):
             # '_hazardbutton',
             '_panel',
             ('_videofile', '_next'),
-            '_status'
+            '_status',
+            '_progress'
             ]
 
         self._video_loaded = False
 
-        if self._args["folder"] is None:
-            if self._args["filepath"] is not None:
-                self.__videoFileSelect(self._args["filepath"][self._current_video])
-
+        self.__videoFileSelect(self._file_list[self._current_video])
         self._hazard_default_duration = 0
 
     def __videoFileSelect(self, filepath):
-        self._videofile.value = str(filepath)
-        self._player.value = self._videofile.value
-        self._player.refresh()
-        self._player.update_frame()
-        # Hazard set to occur for 60 frames upon flagging
-        self._hazard_default_duration = int(self._player.fps * 2)
-        self._video_loaded = True
+        try:
+            self._videofile.value = str(filepath)
+            self._player.value = self._videofile.value
+            self._player.refresh()
+            self._player.update_frame()
+            # Hazard set to occur for 60 frames upon flagging
+            self._hazard_default_duration = int(self._player.fps * 2)
+            self._video_loaded = True
+        except Exception as e:
+            self.__updateStatus("Unable to select video")
 
 
     def __videoFileSelectionEvent(self):
         """
         When the videofile is selected instantiate the video in the player
         """
-        self._player.value = self._videofile.value
-        # Hazard set to occur for 60 frames upon flagging
-        self._hazard_default_duration = int(self._player.fps * 2)
-        self._video_loaded = True
+        try:
+            self._player.value = self._videofile.value
+            # Hazard set to occur for 60 frames upon flagging
+            self._hazard_default_duration = int(self._player.fps * 2)
+            self._video_loaded = True
+        except Exception as e:
+            self.__updateStatus("No video selected")
 
     def __nextVideo(self):
         if self._current_video >= (self._video_count - 1):
             self.__updateStatus("No more videos")
         else:
             self._current_video += 1
-            self.__videoFileSelect(self._args["filepath"][self._current_video])
             self.__reset()
+            self.__videoFileSelect(self._file_list[self._current_video])
+            self._progress.value = self._current_video + 1
 
     def __reset(self):
+        self._player.stop()
         self._timeline.clean()
         self.__updateStatus("")
 
